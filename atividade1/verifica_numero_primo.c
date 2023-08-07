da8684af3072c2140af79783c6d9add34f64aabd
Vithor Félix <91703672+vithorfjm@users.noreply.github.com>
2023-08-07 14:34:55 -0300

    Update verifica_numero_primo.c

--- atividade1/verifica_numero_primo.c
+++ atividade1/verifica_numero_primo.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    int num = (int) argv[1];
     int qntDivisores = 0;
-    for (int i = argv[1] ; i != 1 ; i--) {
-        if (argv[1] % i == 0) {
+    
+    for (int i = num ; i >= 1 ; i--) {
+        if (num % i == 0) {
             qntDivisores++;
         }
     }
-    if (qntDivisores > 2) {
-        printf(0);
+    if (qntDivisores > 2 || num <= 1) {
+        printf("%d", 0);
     } else {
-        printf(1);
+        printf("%d", 1);
     }
+
     return 0;
 }
